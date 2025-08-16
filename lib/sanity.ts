967605550f3060874e4ba6d4bc176f1d0574f485
Waskar Paulino <waskar.paulino@gmail.com>
2025-08-16 14:38:11 -0400

    new layout for mobile

--- lib/sanity.ts
+++ lib/sanity.ts
@@ -10,10 +10,10 @@ export interface CastMember {
   order: number;
 }
 export const client = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'u6u93177',
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
   useCdn: true,
-  apiVersion: '2023-05-03',
+  apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2025-08-10',
 })
 
 const builder = imageUrlBuilder(client)
