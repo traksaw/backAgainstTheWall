5ff3a1a589bf42fe87e8b05b819832750f6908d4
Waskar Paulino <waskar.paulino@gmail.com>
2026-07-11 20:19:54 -0400

    Revert "fix: add missing emailVerified property in User object construction"
    This reverts commit 2d84e4b3ba6f8d4d7bd28abb5d76ba4da7eb4007.

--- components/home/HomeInteractiveShell.tsx
+++ components/home/HomeInteractiveShell.tsx
@@ -28,8 +28,7 @@ export function HomeInteractiveShell({ supporters, children }: HomeInteractiveSh
     _id: rawUser._id,
     email: rawUser.email,
     first_name: rawUser.first_name || '',
-    last_name: rawUser.last_name || '',
-    emailVerified: rawUser.emailVerified || false
+    last_name: rawUser.last_name || ''
   } : null
 
   const profile: Profile | null = rawProfile ? {
